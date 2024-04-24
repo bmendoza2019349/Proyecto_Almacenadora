30cc75a6848241bbef3d102111736288ef5f3f43
Dieguito Marroquin :) <dmarroquin-2019417@kinal.edu.gt>
2024-04-23 20:16:34 -0600

    Se agrega la función eliminar tarea

---
+++ src/helpers/db-validators.js
@@ -0,0 +1,15 @@
+import Tarea from "../tareas/tarea.model.js"; 
+
+export const buscarTareaPorId = async (tareaId) => {
+    try {
+        const tarea = await Tarea.findById(tareaId);
+
+        if (!tarea) {
+            throw new Error("Tarea no encontrada");
+        }
+        return tarea;
+    } catch (error) {
+        throw new Error("Error al buscar la tarea");
+    }
+};
+
