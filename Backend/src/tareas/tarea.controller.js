9717559cf785defa7e5c7a52c177a22fc09d6c0b
Hrodriguez2019188 <hrodriguez-2019188@kinal.edu.gt>
2024-04-23 18:10:44 -0600

    se agrego la primera union del proyecto

---
+++ Backend/src/tareas/tarea.controller.js
@@ -0,0 +1,107 @@
+import Tarea from "../tareas/tarea.model.js";
+
+export const crearTarea = async (req, res) => {
+    try {
+        const { nombre, descripcion, fechaInicio, fechaFinal,
+            estado, persona } = req.body;
+
+        const tarea = new Tarea({
+            nombre,
+            descripcion,
+            fechaInicio,
+            fechaFinal,
+            estado,
+            persona
+        });
+
+        await tarea.save();
+
+        return res.status(200).json({
+            msg: "tarea has been added to database",
+            nombre,
+            fechaInicio,
+            fechaFinal,
+            estado,
+            persona
+
+        });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).send("No se pudo registrar la tarea");
+
+    }
+};
+
+export const editarTarea = async (req, res) => {
+    try {
+        const { nombre, descripcion, fechaInicio, fechaFinal, estado, persona } = req.body;
+        const { id } = req.params;
+        const tarea = await Tarea.findById(id);
+
+    
+
+        if (nombre) tarea.nombre = nombre;
+        if (descripcion) tarea.descripcion = descripcion;
+        if (fechaInicio) tarea.fechaInicio = fechaInicio;
+        if (fechaFinal) tarea.fechaFinal = fechaFinal;
+        if (estado) tarea.estado = estado;
+        if (persona) tarea.persona = persona;
+
+        
+        await tarea.save();
+
+        return res.status(200).json({
+            msg: "Tarea actualizada correctamente",
+            tarea
+        });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).send("No se pudo actualizar la tarea");
+    }
+};
+
+export const mostrarTodasLasTareas = async (req, res) => {
+    try {
+        const tareas = await Tarea.find();
+
+        if (!tareas || tareas.length === 0) {
+            return res.status(404).json({ msg: "No se encontraron tareas" });
+        }
+
+        return res.status(200).json({
+            msg: "Lista de todas las tareas",
+            tareas
+        });
+    } catch (error) {
+
+        console.error(error);
+        return res.status(500).send("Error al obtener las tareas");
+    }
+};
+
+
+export const cambiarEstadoTarea = async (req, res) => {
+    try {
+        const { id } = req.params; 
+        const tarea = await Tarea.findById(id); 
+
+        if (!tarea) {
+            return res.status(404).json({ msg: "Tarea no encontrada" });
+        }
+
+      
+        tarea.estado = tarea.estado === 'Pendiente' ? 'Completado' : 'Pendiente';
+
+
+        await tarea.save();
+
+        return res.status(200).json({
+            msg: `El estado de la tarea ha sido cambiado a ${tarea.estado}`,
+            tarea
+        });
+    } catch (error) {
+
+        console.error(error);
+        return res.status(500).send("No se pudo cambiar el estado de la tarea");
+    }
+};
---
+++ Backend/src/tareas/tarea.routes.js
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import { check } from "express-validator";
+import { crearTarea, editarTarea, cambiarEstadoTarea, mostrarTodasLasTareas } from "./tarea.controller.js"
+import { validarCampos } from "../middlewares/validar-campos.js";
+import {buscarTareaPorId } from '../helpers/db-validators.js'
+
+const router = Router();
+
+router.post(
+    "/agregar",
+    [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('descripcion', 'La descripcion es obligatorio').not().isEmpty(),
+        check('fechaInicio', 'La fecha de Inicio es obligatorio').not().isEmpty(),
+        check('fechaFinal', 'La fecha Final es obligatorio').not().isEmpty(),
+        check('estado', 'El estado es obligatorio').not().isEmpty(),
+        check('persona', 'El nombre completo de la persona es obligatorio').not().isEmpty(),
+        validarCampos,
+    ], crearTarea);
+
+    router.put(
+        "/editar/:id",
+        [
+            check("id", "Id no valido").isMongoId(),
+            check('id').custom(buscarTareaPorId),
+            validarCampos,
+        ], 
+        editarTarea
+    );
+
+    router.get(
+        "/listar",
+        [
+        ],mostrarTodasLasTareas
+    );
+    
+    router.put(
+        "/editarEstado/:id",
+        [], cambiarEstadoTarea
+    );
+
+export default router;
