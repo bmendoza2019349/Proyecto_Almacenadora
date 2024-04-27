9b7236382da55c41ad5039686ed24d468f3a670f
bmendoza-2019349 <bmendoza-2019349@kinal.edu.gt>
2024-04-27 16:23:00 -0600

    merge en la rama

--- Backend/src/helpers/db-validators.js
+++ Backend/src/helpers/db-validators.js
@@ -1,2 +1,14 @@
+import Tarea from "../tareas/tarea.model.js"; 
 
+export const buscarTareaPorId = async (tareaId) => {
+    try {
+        const tarea = await Tarea.findById(tareaId);
 
+        if (!tarea) {
+            throw new Error("Tarea no encontrada");
+        }
+        return tarea;
+    } catch (error) {
+        throw new Error("Error al buscar la tarea");
+    }
+};
